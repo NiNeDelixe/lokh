9450a560add47bd39fe510dc89f1b622abda55e7
Template Janitor <template.janitor@example.com>
2026-05-01 20:40:42 +0000

    Cleanup template and initialize repository

--- cmake/Linker.cmake
+++ cmake/Linker.cmake
@@ -1,20 +1,20 @@
-macro(myproject_configure_linker project_name)
-  set(myproject_USER_LINKER_OPTION
+macro(lokh_configure_linker project_name)
+  set(lokh_USER_LINKER_OPTION
     "DEFAULT"
       CACHE STRING "Linker to be used")
-    set(myproject_USER_LINKER_OPTION_VALUES "DEFAULT" "SYSTEM" "LLD" "GOLD" "BFD" "MOLD" "SOLD" "APPLE_CLASSIC" "MSVC")
-  set_property(CACHE myproject_USER_LINKER_OPTION PROPERTY STRINGS ${myproject_USER_LINKER_OPTION_VALUES})
+    set(lokh_USER_LINKER_OPTION_VALUES "DEFAULT" "SYSTEM" "LLD" "GOLD" "BFD" "MOLD" "SOLD" "APPLE_CLASSIC" "MSVC")
+  set_property(CACHE lokh_USER_LINKER_OPTION PROPERTY STRINGS ${lokh_USER_LINKER_OPTION_VALUES})
   list(
     FIND
-    myproject_USER_LINKER_OPTION_VALUES
-    ${myproject_USER_LINKER_OPTION}
-    myproject_USER_LINKER_OPTION_INDEX)
+    lokh_USER_LINKER_OPTION_VALUES
+    ${lokh_USER_LINKER_OPTION}
+    lokh_USER_LINKER_OPTION_INDEX)
 
-  if(${myproject_USER_LINKER_OPTION_INDEX} EQUAL -1)
+  if(${lokh_USER_LINKER_OPTION_INDEX} EQUAL -1)
     message(
       STATUS
-        "Using custom linker: '${myproject_USER_LINKER_OPTION}', explicitly supported entries are ${myproject_USER_LINKER_OPTION_VALUES}")
+        "Using custom linker: '${lokh_USER_LINKER_OPTION}', explicitly supported entries are ${lokh_USER_LINKER_OPTION_VALUES}")
   endif()
 
-  set_target_properties(${project_name} PROPERTIES LINKER_TYPE "${myproject_USER_LINKER_OPTION}")
+  set_target_properties(${project_name} PROPERTIES LINKER_TYPE "${lokh_USER_LINKER_OPTION}")
 endmacro()
