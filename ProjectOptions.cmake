c19d49d4cb4b9b913f52e632c83e27adb8a67545
Template Janitor <template.janitor@example.com>
2026-05-01 20:41:31 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(lokh_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -58,125 +58,125 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(lokh_setup_options)
+  option(lokh_ENABLE_HARDENING "Enable hardening" ON)
+  option(lokh_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    lokh_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    lokh_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  lokh_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR lokh_PACKAGING_MAINTAINER_MODE)
+    option(lokh_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(lokh_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(lokh_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(lokh_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(lokh_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(lokh_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(lokh_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(lokh_ENABLE_CACHE "Enable ccache" OFF)
   elseif(ENABLE_DEVELOPER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(lokh_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(lokh_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(lokh_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(lokh_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(lokh_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(lokh_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(lokh_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(lokh_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(lokh_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(lokh_ENABLE_CACHE "Enable ccache" ON)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(lokh_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(lokh_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(lokh_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(lokh_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(lokh_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(lokh_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(lokh_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(lokh_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(lokh_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_LIZARD
-      myproject_ENABLE_BLOATY
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      lokh_ENABLE_IPO
+      lokh_WARNINGS_AS_ERRORS
+      lokh_ENABLE_SANITIZER_ADDRESS
+      lokh_ENABLE_SANITIZER_LEAK
+      lokh_ENABLE_SANITIZER_UNDEFINED
+      lokh_ENABLE_SANITIZER_THREAD
+      lokh_ENABLE_SANITIZER_MEMORY
+      lokh_ENABLE_UNITY_BUILD
+      lokh_ENABLE_CLANG_TIDY
+      lokh_ENABLE_CPPCHECK
+      lokh_ENABLE_LIZARD
+      lokh_ENABLE_BLOATY
+      lokh_ENABLE_COVERAGE
+      lokh_ENABLE_PCH
+      lokh_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  lokh_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (lokh_ENABLE_SANITIZER_ADDRESS OR lokh_ENABLE_SANITIZER_THREAD OR lokh_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(lokh_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(lokh_global_options)
+  if(lokh_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    lokh_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  lokh_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(lokh_ENABLE_HARDENING AND lokh_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR lokh_ENABLE_SANITIZER_UNDEFINED
+       OR lokh_ENABLE_SANITIZER_ADDRESS
+       OR lokh_ENABLE_SANITIZER_THREAD
+       OR lokh_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${lokh_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${lokh_ENABLE_SANITIZER_UNDEFINED}")
+    lokh_enable_hardening(lokh_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(lokh_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(lokh_warnings INTERFACE)
+  add_library(lokh_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  lokh_set_project_warnings(
+    lokh_warnings
+    ${lokh_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -187,74 +187,74 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    lokh_enable_sanitizers(
+      lokh_options
+      ${lokh_ENABLE_SANITIZER_ADDRESS}
+      ${lokh_ENABLE_SANITIZER_LEAK}
+      ${lokh_ENABLE_SANITIZER_UNDEFINED}
+      ${lokh_ENABLE_SANITIZER_THREAD}
+      ${lokh_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(lokh_options PROPERTIES UNITY_BUILD ${lokh_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(lokh_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      lokh_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(lokh_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    lokh_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(lokh_ENABLE_CLANG_TIDY)
+    lokh_enable_clang_tidy(lokh_options ${lokh_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(lokh_ENABLE_CPPCHECK)
+    lokh_enable_cppcheck(${lokh_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
   
-  if(myproject_ENABLE_LIZARD)
-    myproject_enable_lizard(${myproject_WARNINGS_AS_ERRORS})
+  if(lokh_ENABLE_LIZARD)
+    lokh_enable_lizard(${lokh_WARNINGS_AS_ERRORS})
   endif()
   
-  if(myproject_ENABLE_BLOATY)
-    myproject_enable_bloaty()
+  if(lokh_ENABLE_BLOATY)
+    lokh_enable_bloaty()
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(lokh_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    lokh_enable_coverage(lokh_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(lokh_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(lokh_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(lokh_ENABLE_HARDENING AND NOT lokh_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR lokh_ENABLE_SANITIZER_UNDEFINED
+       OR lokh_ENABLE_SANITIZER_ADDRESS
+       OR lokh_ENABLE_SANITIZER_THREAD
+       OR lokh_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    lokh_enable_hardening(lokh_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
